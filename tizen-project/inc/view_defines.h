39967112068b7a0f94a814d297d983da00fb8fa6
Cédric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2021-02-01 22:29:25 +0700

    Add date and simple battery level. (#2)

--- tizen-project/inc/view_defines.h
+++ tizen-project/inc/view_defines.h
@@ -2,5 +2,10 @@
 #define _VIEW_DEFINES_H
 
 #define MSG_ID_SET_TIME 1
-#define MSG_ID_AMBIENT_MODE 2
+#define MSG_ID_SET_DATE 2
+#define MSG_ID_AMBIENT_MODE 3
+#define PART_DATE_DAY "part_date_day"
+#define PART_DATE_MONTH "part_date_month"
+#define PART_BATTERY "part_battery"
+
 #endif
